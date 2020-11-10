45013274448410e9613f0d7adb81b68af8b5c29a
travisbrigman <travis1000@gmail.com>
2020-11-10 14:56:14 -0600

    working on stuff

--- rareapi/views/category.py
+++ rareapi/views/category.py
@@ -1,13 +1,35 @@
 """View module for handling requests about categories"""
+from django.core.exceptions import ValidationError
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status
 from rareapi.models import Category
 
 
 class Categories(ViewSet):
     """Rare Categories"""
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns:
+            Response -- JSON serialized game instance
+        """
+
+        category = Category.objects.get(user=request.auth.user)
+
+        category = Category()
+        Category.label = request.data["label"]
+
+        try:
+            category.save()
+            serializer = CategorySerializer(category, context={'request': request})
+            return Response(serializer.data)
+
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
         """Handle GET requests to get all the categories
