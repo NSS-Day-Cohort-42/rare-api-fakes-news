76723e603b404cead2a63d425eb4e428f57a6457
estherviolin <esthernaomi.violin@gmail.com>
2020-11-12 10:14:29 -0600

    view, create, delete reactionposts

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,6 +1,7 @@
 # insert imports here
 from .auth import login_user, register_user
 from .category import Categories
+from .postreaction import PostReactions
 from .post import Posts
 from .posttag import PostTags
 from .reaction import Reactions
