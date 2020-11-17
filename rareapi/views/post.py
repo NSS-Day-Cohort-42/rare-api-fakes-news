c051136f62c1b70f07ddb55e1374b906dadcf796
travisbrigman <travis1000@gmail.com>
2020-11-17 09:35:01 -0600

    working on post approval

--- rareapi/views/post.py
+++ rareapi/views/post.py
@@ -130,7 +130,7 @@ class Posts(ViewSet):
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 #TODO:change to PUT
-    @action(methods=['patch'], detail=True)
+    @action(methods=['PUT'], detail=True)
     def approve(self, request, pk=None):
 
         post = Post.objects.get(pk=pk)
