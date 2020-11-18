4fdca2baf18a144809ddfecdb2f48a1b01744f61
cwelshofer <cwelshofer@gmail.com>
2020-11-18 10:17:48 -0600

    working tracking

--- rareapi.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM rareapi_postINSERT INTO rareapi_postreaction (id, post_id, reaction_id, user_id)
-VALUES (
-    id:integer,
-    post_id:integer,
-    reaction_id:integer,
-    user_id:integer
-  );
-DELETE  FROM rareapi_postreaction
